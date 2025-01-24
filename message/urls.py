38fdc3b83295a50def853deafca83a6f060b03a2
Paused for 0.09 minutes <Paused for 1.2 minutes>
2025-01-24 11:22:22 +0530

    fixed message read status

--- message/urls.py
+++ message/urls.py
@@ -3,8 +3,7 @@ from .views import MessageViewSet
 
 urlpatterns = [
     path('', MessageViewSet.as_view({'get': 'get_messages'})),  # for listing and creating campaigns
-    path('<int:pk>/', MessageViewSet.as_view({'patch': 'update'})),
-    #
+    path('<int:pk>/', MessageViewSet.as_view({'delete': 'update'})),
     # path('sequence', CampaignSequenceViewSet.as_view({'get': 'list', 'post': 'create'})),  # for listing and creating campaigns
     # path('sequence/<int:pk>/', CampaignSequenceViewSet.as_view({'get': 'retrieve', 'patch': 'update', 'delete': 'destroy'})),
 
