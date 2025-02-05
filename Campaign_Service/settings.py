94a8f8590bb7db35e363c0fc46275b7616baf858
Paused for 0.09 minutes <Paused for 1.2 minutes>
2025-02-05 12:59:43 +0530

    updated allowed host to fetch from .env

--- Campaign_Service/settings.py
+++ Campaign_Service/settings.py
@@ -26,10 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Application definition
 
