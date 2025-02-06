94c95f0a3e6a92557a4ae959fe49047be6214267
Paused for 0.09 minutes <Paused for 1.2 minutes>
2025-02-06 12:17:43 +0530

    updated cors allowed, set allow all to false

--- Campaign_Service/settings.py
+++ Campaign_Service/settings.py
@@ -45,7 +45,17 @@ INSTALLED_APPS = [
     'practice',
     'corsheaders',
 ]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ALLOWED_ORIGINS = [
+   origin.strip()
+   for origin in os.environ.get(
+       'CORS_ALLOWED_ORIGINS',
+       'http://localhost:5173'
+   ).split(',')
+]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
